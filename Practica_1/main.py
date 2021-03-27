c402a67addf93a5b31484e472d1dee8743acaa3a
carloshermida <carlos.hermida@udc.es>
2021-03-27 10:47:17 +0100

    Main v1.1.0

--- Practica_1/main.py
+++ Practica_1/main.py
@@ -1,9 +1,13 @@
-# Práctica 1
-# Carlos Hermida | Clara Lado
-# Main
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Practica 1
+Carlos Hermida Clara Lado
+Main
+"""
 
 from calculus import iniciar
 
 if __name__ == "__main__":
     
-    iniciar()
+    print(iniciar())
