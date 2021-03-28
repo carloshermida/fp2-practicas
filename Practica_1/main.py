42891bd69222c4d66bb8db5ce60c24dbbe298fde
carloshermida <carlos.hermida@udc.es>
2021-03-28 23:51:00 +0200

    main v2.2.0

--- Practica_1/main.py
+++ Practica_1/main.py
@@ -10,4 +10,18 @@ from calculus import iniciar
 
 if __name__ == "__main__":
     
-    print("RESULTADO", "_"*(30-len("RESULTADO")), iniciar())
+    print("*"*20, "CALCULADORA", "*"*30, "\n\n1) Iniciar\n2) Manual de usuario")
+    opcion = int(input("> "))
+    if opcion == 1:
+        print("RESULTADO", "_"*(30-len("RESULTADO")), iniciar())
+    elif opcion == 2:
+        print("="*35, "\n", "\t\tManual de usuario", "\n", "="*35, "\n", sep="")
+      
+        # Manual de usuario:
+        print("\
+Para calcular el resultado de una expresión debe escribirla\n\
+sin espacios.1")
+   
+    
+    else:
+        print("Opción no válida")
