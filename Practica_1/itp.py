29abd8a4688671bad14315b42577d4655910826b
carloshermida <carlos.hermida@udc.es>
2021-03-28 22:10:57 +0200

    itp 2.0.0

--- Practica_1/itp.py
+++ Practica_1/itp.py
@@ -10,29 +10,35 @@ from stack import ArrayStack as Stack
 
 def infixToPostfix(infixexpr):
     prec = {}
+    prec["t"], prec["c"], prec["s"], prec["r"] = 4, 4, 4, 4
     prec["*"] , prec["/"] = 3, 3
     prec["+"] , prec["-"] = 2, 2
     prec["("] = 1
     opStack = Stack()
     postfixList = []
     tokenList = infixexpr.split(" ")
+    
 
     for token in tokenList:
-        if token.isalpha() or token.isnumeric():
+        if "." in token:
+            decimal_lista = token.split(".")
+            divisor = ("1"+"0"*len(decimal_lista[1]))
+            token = str(decimal_lista[0]+decimal_lista[1]+" "+divisor+" /")
             postfixList.append(token)
-        elif token == '(':
-            opStack.push(token)
-        elif token == ')':
-            topToken = opStack.pop()
-            while topToken != '(':
-                postfixList.append(topToken)
-                topToken = opStack.pop()
         else:
-            while (not opStack.is_empty()) and \
-               (prec[opStack.peek()] >= prec[token]):
-                  postfixList.append(opStack.pop())
-            opStack.push(token)
-
+            if token.isnumeric():
+                postfixList.append(token)
+            elif token == '(':
+                opStack.push(token)
+            elif token == ')':
+                topToken = opStack.pop()
+                while topToken != '(':
+                    postfixList.append(topToken)
+                    topToken = opStack.pop()
+            else:
+                while (not opStack.is_empty()) and (prec[opStack.peek()] >= prec[token]):
+                    postfixList.append(opStack.pop())
+                opStack.push(token)
     while not opStack.is_empty():
         postfixList.append(opStack.pop())
     return " ".join(postfixList)
