2a2a841ff392b19c7b09a88401c95ec96cfd3629
carloshermida <carlos.hermida@udc.es>
2021-03-28 22:09:14 +0200

    calculus 2.0.0

--- Practica_1/calculus.py
+++ Practica_1/calculus.py
@@ -9,32 +9,39 @@ Modulo Calculos
 from writingcheck import espaciador, check_brackets
 from itp import infixToPostfix
 from stack import ArrayStack as Stack
-import math
-
+from math import tan, sin, cos, sqrt
 
 def operador(op, num1, num2):
     if op == "+":
-        resultado = num1 + num2
-        return resultado
+        return num1 + num2
     
     elif op == "-":
-        resultado = num1 - num2
-        return resultado 
+        return num1 - num2 
     
     elif op == "*":
-        resultado = num1 * num2
-        return resultado 
+        return num1 * num2 
     
     elif op == "/":
-        resultado = num1/num2
-        return resultado
+        return num1/num2
     
     elif op == "**":
-        resultado = num1**num2
-        return resultado 
+        return num1**num2
     
     else:
         return "Operando no valido"
+
+def operador_especial(op, num):
+    if op == "t":
+        return tan(num)
+    
+    elif op == "s":
+        return sin(num)
+    
+    elif op == "c":
+        return cos(num)
+    
+    elif op == "r":
+        return sqrt(num)
 
 def iniciar(): 
     infijo = input("Introduce una expresion infija: ")
@@ -43,40 +50,26 @@ def iniciar():
         infijo = espaciador(infijo)
         if infijo != "ERROR":
             postfijo = infixToPostfix(infijo)
+            print("POSTFIJO", "_"*(30-len("POSTFIJO")), postfijo)
             lista_postfijo = postfijo.split(" ")
             simbolos = ["+", "-", "*", "/", "**"]
+            especial = ["t", "c", "s", "r"]
             pila = Stack()
     
             for item in lista_postfijo:
                 pila.push(item)
-                # comprueba si el elemento empieza por seno para poder identificarlo
-                if (item.startswith("sin(") or item.startswith("sen(")) and item.endswith(")"):
-                    # mira en que posicion esta el parentesis de cierre para que coja todo lo de dentro
-                    posicion = item.index(")")
-                    #se coje todo lo que esta entre ambos aprentesis
-                    m = item[4:posicion]
-                    # se le aplica la funcion a eso 
-                    result = math.sin(m)
-                    #cargamos el resultado en la pila
-                    pila.push(result)
-                elif item.startswith("cos(") and item.endswith(")"):
-                    posicion = item.index(")")
-                    m = item[4:posicion]
-                    result = math.cos(m)
-                    pila.push(result)
-                elif (item.startswith("tan(") or item.startswith("tg(")) and item.endswith(")"):
-                    posicion = item.index(")")
-                    m = item[4:posicion]
-                    result = math.tan(m)
-                    pila.push(result)
-                
                 if item in simbolos:
                     op = pila.pop()
-                    num2 = int(pila.pop())
+                    num2 = float(pila.pop())
                     if op == "/" and num2 == 0:
                         return "ERROR"
-                    num1 = int(pila.pop())
-                    pila.push(int(operador(op, num1, num2)))
+                    num1 = float(pila.pop())
+                    pila.push(float(operador(op, num1, num2)))
+                
+                if item in especial:
+                    op = pila.pop()
+                    num = float(pila.pop())
+                    pila.push(float(operador_especial(op, num)))
         
             return(pila.peek())
     
