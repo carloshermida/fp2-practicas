341c68847a07d3be4cec04329da76668aedbd75d
carloshermida <carlos.hermida@udc.es>
2021-04-08 09:48:21 +0200

    main v0.0.0

---
+++ Practica_2/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Main
+# Carlos Hermida / Clara Lado
+
+"""
+Código principal de la Práctica 2.
+"""
+
+import sys
+
+if __name__ == "__main__":
+   
+    print("sys: ", sys.argv)
