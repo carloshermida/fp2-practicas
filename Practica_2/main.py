72c8cb78ebcc7ff754ddf5e3ce11dae254c04b62
carloshermida <carlos.hermida@udc.es>
2021-04-17 15:43:51 +0200

    main v2.0.0

--- Practica_2/main.py
+++ Practica_2/main.py
@@ -12,19 +12,21 @@ import sys
 from stockist import seleccionadorvacuna
 from add import llegadadosis, llegadapersonas
 from array_queue import ArrayQueue as Queue
+from categories import Vacuna
 
 def main():
     #intento que lo solicite en el orden dosis A, B, C y Nº de pers
     if len(sys.argv) != 6:
+        print("Formato:\n<Cantidad Vacuna A / día>\n<Cantidad Vacuna B / día>\n<Cantidad Vacuna C / día>\n<Personas nuevas / día>\n<Días>")
         sys.exit()
         #si no se aportan todos los argumentos que se cierre
     
     try:
         #identifico cada elemento introducido
-        vac_a = int(sys.argv[1])
-        vac_b = int(sys.argv[2])    
-        vac_c = int(sys.argv[3])
-        npers = int(sys.argv[4])
+        vac_a = Vacuna("a", 71, 101, int(sys.argv[1]), 0, 0, 0)
+        vac_b = Vacuna("", 51, 70, int(sys.argv[2]), 0, 0, 0) 
+        vac_c = Vacuna("c", 0, 50, int(sys.argv[3]), 0, 0, 0)
+        personas_dia = int(sys.argv[4])
         days = int(sys.argv[5])
         #llamo a las funciones para crear las colas
         
@@ -34,22 +36,21 @@ def main():
                 dosis_b = Queue() #para entre 50 y 70
                 dosis_c = Queue() #para menores de 50
                 pacientes = Queue()
-                resumen_final = []
                 
             llegadadosis(vac_a, vac_b, vac_c, dosis_a, dosis_b, dosis_c)
-            llegadapersonas(npers, pacientes)
-            resumen_final = seleccionadorvacuna(dosis_a, dosis_b, dosis_c, pacientes, day, resumen_final)
-    
+            llegadapersonas(personas_dia, pacientes)
+            seleccionadorvacuna(dosis_a, dosis_b, dosis_c, pacientes, day, vac_a, vac_b, vac_c)
+
+   
     finally:
-        print("RESUMEN FINAL:\n\n\tJOVENES VACUNADOS: {}\n\tJOVENES NO VACUNADOS: {}\n\t\
-ADULTOS VACUNADOS: {}\n\tADULTOS NO VACUNADOS: {}\n\tANCIANOS VACUNADOS: {}\n\t\
-ANCIANOS NO VACUNADOS: {}\n\n\tTOTAL VACUNADOS: {}\n\tTOTAL NO VACUNADOS: {}\n\n\t\
-VACUNAS SOBRANTES:\n\t\tA: {}\n\t\tB: {}\n\t\tC: {}".format(resumen_final[0],
-resumen_final[1],resumen_final[2], resumen_final[3],resumen_final[4],resumen_final[5],
-(resumen_final[0]+resumen_final[2]+resumen_final[4]), (resumen_final[1]+resumen_final[3]+resumen_final[5]),
+        print("RESUMEN:\n\n\tA VACUNADOS: {}\n\tA NO VACUNADOS: {}\n\t\
+B VACUNADOS: {}\n\tB NO VACUNADOS: {}\n\tC VACUNADOS: {}\n\t\
+C NO VACUNADOS: {}\n\n\tTOTAL VACUNADOS: {}\n\tTOTAL NO VACUNADOS: {}\n\n\t\
+VACUNAS SOBRANTES:\n\t\tA: {}\n\t\tB: {}\n\t\tC: {}".format(vac_a.getVacunados(),
+vac_a.getNoVacunados(),vac_b.getVacunados(), vac_b.getNoVacunados(),vac_c.getVacunados(),vac_c.getNoVacunados(),
+(vac_a.getVacunados()+vac_b.getVacunados()+vac_c.getVacunados()), (vac_a.getNoVacunados()+vac_b.getNoVacunados()+vac_c.getNoVacunados()),
 len(dosis_a), len(dosis_b), len(dosis_c)))
          
-
 if __name__ == "__main__":
    
     
