9fa99e4414d4b56da320d626ef907ef5c3b469cc
carloshermida <carlos.hermida@udc.es>
2021-04-17 15:42:37 +0200

    add v2.0.0

--- Practica_2/add.py
+++ Practica_2/add.py
@@ -8,6 +8,7 @@
 Definición de las funciones para añadir
 vacunas, personas
 """
+from categories import Vacuna
 from array_queue import ArrayQueue as Queue
 import random
 
@@ -16,25 +17,31 @@ dosis_b = Queue()
 dosis_c = Queue()
 pacientes = Queue()
 
-def llegadadosis(x, y, z, dosis_a, dosis_b, dosis_c):
+def llegadadosis(vac_a, vac_b, vac_c, dosis_a, dosis_b, dosis_c):
     """Funcion encargada de dar un numero
     de vacunas de cada tipo."""
    
     #hago un bucle para que cada cola se llene del numero de vacunas llegadas
-    for i in range(x):
+    for i in range(vac_a.getEntrada()):
         dosis_a.enqueue(1)
 
-    for i in range(y):
+    for i in range(vac_b.getEntrada()):
         dosis_b.enqueue(1)
     
-    for i in range(z):
+    for i in range(vac_c.getEntrada()):
         dosis_c.enqueue(1)
-
     
-def llegadapersonas(npers, pacientes):
+    vac_a.setStock(len(dosis_a))
+    vac_b.setStock(len(dosis_b))
+    vac_c.setStock(len(dosis_c))
+    
+    
+def llegadapersonas(personas_dia, pacientes):
+
    """Funcion encargada de crear la cola de gente de edades aleatorias uniformemente"""
    #creo una cola de personas con edades aleatorias
    
-   for i in range(npers):
+   for i in range(personas_dia):
        pacientes.enqueue(random.randint(0, 100))
-         
+
+    # aqui hay que hacer camios para implementar sample.py
