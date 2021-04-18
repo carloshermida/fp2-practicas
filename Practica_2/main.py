7b7b5a67168af6ca87f6526495a05d2639368d02
carloshermida <carlos.hermida@udc.es>
2021-04-18 13:00:15 +0200

    main v3.0.0

--- Practica_2/main.py
+++ Practica_2/main.py
@@ -15,43 +15,71 @@ from array_queue import ArrayQueue as Queue
 from categories import Vacuna
 
 def main():
-    #intento que lo solicite en el orden dosis A, B, C y Nº de pers
-    if len(sys.argv) != 6:
-        print("Formato:\n<Cantidad Vacuna A / día>\n<Cantidad Vacuna B / día>\n<Cantidad Vacuna C / día>\n<Personas nuevas / día>\n<Días>")
+    """ Función principal encargada de obtener los parámetros
+    del usuario y ejecutar el código tantas veces como días de simulación """
+    
+    # Leemos los argumentos de entrada
+    personas_dia = int(sys.argv[1])
+    days = int(sys.argv[2])
+    
+    #-------------------------------------------------------------------------
+    # Tipos de vacunas
+    #-------------------------------------------------------------------------
+    vac_a = Vacuna("a", 71, 101, int(sys.argv[3]), 0, 0, 0)
+    vac_b = Vacuna("b", 50, 70, int(sys.argv[4]), 0, 0, 0)
+    vac_c = Vacuna("c", 0, 49, int(sys.argv[5]), 0, 0, 0)
+    #-------------------------------------------------------------------------
+    
+    vac_lista = []
+    dosis_lista = []
+    cadena_var = ""
+    
+    # Añadimos a la lista de vacunas las vacunas que creamos y al la lista de 
+    # dosis sus respectivas colas de dosis. De esta forma obtenemos una lista
+    # con todas las variables vacuna, y otra lista con las variales dosis que
+    # definiremos más tarde
+   
+    # (Vacuna.lista_vacunas devuelve una lista con los nombres de todos los objectos vacuna creados)
+    for item in Vacuna.lista_vacunas:
+        vac_lista.append("vac_" + item)
+        dosis_lista.append("dosis_" + item)
+        # Creamos un string con todas las variables separadas por coma para facilitar
+        # la llamada de funciones posteriormente
+        cadena_var += ("vac_" + item + ",dosis_" + item)
+        # Añadimos la coma en despues de cada par, a excepción del último
+        if Vacuna.lista_vacunas.index(item) != len(Vacuna.lista_vacunas)-1:
+            cadena_var += (",")
+    
+    # Si hay más o menos argumentos de los requeridos según la cantidad de vacunas,
+    # el programa se cerrará
+    if len(sys.argv) != (len(vac_lista) + 3):
+        print("Formato:\n<Personas nuevas / día>\n<Días>\n<Cantidad Vacuna A / día>\n<Cantidad Vacuna B / día>\n<Cantidad Vacuna C / día> ...")
         sys.exit()
-        #si no se aportan todos los argumentos que se cierre
-    
-    try:
-        #identifico cada elemento introducido
-        vac_a = Vacuna("a", 71, 101, int(sys.argv[1]), 0, 0, 0)
-        vac_b = Vacuna("", 51, 70, int(sys.argv[2]), 0, 0, 0) 
-        vac_c = Vacuna("c", 0, 50, int(sys.argv[3]), 0, 0, 0)
-        personas_dia = int(sys.argv[4])
-        days = int(sys.argv[5])
-        #llamo a las funciones para crear las colas
+       
+    # Comenzamos el bucle en el día 1, hasta el día especificado por el usuario    
+    for day in range(1, days+1):
         
-        for day in range(1, days+1):
-            if day == 1:
-                dosis_a = Queue() #para mayores de 70
-                dosis_b = Queue() #para entre 50 y 70
-                dosis_c = Queue() #para menores de 50
-                pacientes = Queue()
-                
-            llegadadosis(vac_a, vac_b, vac_c, dosis_a, dosis_b, dosis_c)
-            llegadapersonas(personas_dia, pacientes)
-            seleccionadorvacuna(dosis_a, dosis_b, dosis_c, pacientes, day, vac_a, vac_b, vac_c)
+        if day == 1:
+            # Si es el primer día, inicializamos las colas de dosis, cuyos nombres
+            # están espcificados en la lista de dosis
+            for item in dosis_lista:
+                globals()[item] = Queue()
+            # Inicializamos también la cola de pacientes
+            pacientes = Queue()
+        
+        # Llamamos a la función llegadadosis a través de la función eval para
+        # poder mandarle los parámetros de todas las vacunas y dosis.
+        eval("llegadadosis" + "({})".format(cadena_var))
+        # Generamos personas con edades aleatorias
+        llegadapersonas(personas_dia, pacientes)
+        # Procedemos de la misma forma que con la función llegadadosis,
+        # aunque en este caso añadimos 3 parametros fijos que se mandarán
+        # a la función seleccionadorvacuna si importar el número de vacunas
+        eval("seleccionadorvacuna" + "(pacientes, day, days, {})".format(cadena_var))
 
-   
-    finally:
-        print("RESUMEN:\n\n\tA VACUNADOS: {}\n\tA NO VACUNADOS: {}\n\t\
-B VACUNADOS: {}\n\tB NO VACUNADOS: {}\n\tC VACUNADOS: {}\n\t\
-C NO VACUNADOS: {}\n\n\tTOTAL VACUNADOS: {}\n\tTOTAL NO VACUNADOS: {}\n\n\t\
-VACUNAS SOBRANTES:\n\t\tA: {}\n\t\tB: {}\n\t\tC: {}".format(vac_a.getVacunados(),
-vac_a.getNoVacunados(),vac_b.getVacunados(), vac_b.getNoVacunados(),vac_c.getVacunados(),vac_c.getNoVacunados(),
-(vac_a.getVacunados()+vac_b.getVacunados()+vac_c.getVacunados()), (vac_a.getNoVacunados()+vac_b.getNoVacunados()+vac_c.getNoVacunados()),
-len(dosis_a), len(dosis_b), len(dosis_c)))
-         
-if __name__ == "__main__":
-   
+
     
-   main()
+if __name__ == "__main__":
+    
+    main()
+    
