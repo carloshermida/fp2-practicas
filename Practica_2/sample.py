d28fedafeb900a178ca440bf43cf6fec3fa690ba
carloshermida <carlos.hermida@udc.es>
2021-04-18 16:48:07 +0200

    sample v4.0.0

--- Practica_2/sample.py
+++ Practica_2/sample.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+# -- coding: utf-8 --
 
 # Módulo Muestra
 # Carlos Hermida / Clara Lado
@@ -9,33 +9,47 @@ Definición de las funciones
 para el muestreo
 """
 
-def galicia_sample():
+import random
+from array_queue import ArrayQueue as Queue
 
+pacientes = Queue()
+
+def distribucion_galicia(personas_dia, pacientes):
+    """Función que añade personas de edad aleatoria según la probabilidad gallega"""
+    
+    # Se abre el archivo con los datos de la población gallega y se lee su contenido
     with open("galicia_data.txt", 'r') as f:
         contenido = f.read()
- 
+    
+    # Se divide en lineas el contenido
     data = contenido.split("\n")  
-
+    
+    # Se elimina el primer elemento que no aporta nada
     data.pop(0)
+    
+    # Se almacena el numero total de personas en una constante para trabajar con el posteriormente
     total = int(data.pop(0))
-
-    probability = dict()
+    
+    # Creamos una lista con las edades y otra con las probabilidades
+    edades = []
+    probabilidades = []
     i = 0
     j = 1
-
-    while len(probability) < 101:
-        probability[int(data[i])] = int(data[j])/total
+    
+    # Creamos un bucle que vaya recorriendo todo el archivo
+    while len(edades) < 101:
+        edades.append(int(data[i]))
+        probabilidades.append(int(data[j])/total)
         i += 2
         j +=2
+
+    # Añadimos el número de personas requerido
+    for i in range(personas_dia):
+        pacientes.enqueue((random.choices(edades, probabilidades)[0]))
+
+def distribucion_aleatoria(personas_dia, pacientes):
+    """Función que añade personas de edad aleatoria equiprobables"""
     
-    return probability
-
-
-
-# No está acabada la función, ahora que tenemos las probbilidades de cada edad hay que 
-#escoger gente seun los datos
-
-if __name__ == "__main__":
-    
-    prob = galicia_sample()
-    print(prob)
+    # Añadimos el número de personas requerido
+    for i in range(personas_dia):
+        pacientes.enqueue(random.randint(0, 100))
