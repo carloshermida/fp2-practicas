9a51d69a8781e6f695febe79e03d748bf1dc1b70
carloshermida <carlos.hermida@udc.es>
2021-04-18 12:46:04 +0200

    add v3.0.0

--- Practica_2/add.py
+++ Practica_2/add.py
@@ -17,30 +17,43 @@ dosis_b = Queue()
 dosis_c = Queue()
 pacientes = Queue()
 
-def llegadadosis(vac_a, vac_b, vac_c, dosis_a, dosis_b, dosis_c):
+def llegadadosis(*arg):
     """Funcion encargada de dar un numero
     de vacunas de cada tipo."""
+    
+    # Tras recibir una tupla con un número indefinido de variables, 
+    # las almacenamos en la lista var, para poder trabajar cómodamente
+    var = []
+    for item in arg:
+        var.append(item)
+    
+    # Ejcutamos tantas veces como vacunas existan
+    for i in range(len(var)//2):
+        # La vacuna será el primer elemento y su cola de dosis correspondiente
+        # será el siguiente elemento (según lo deifinimos en el string cadena_var
+        # en el main)
+        vac = var[0]
+        dosis = var[1]
+        
+        # Añadimos a la cola de dosis las unidades especificadas en la entrada
+        # diaria de cada vacuna
+        for j in range(vac.getEntrada()):
+            dosis.enqueue(1)
+        
+        # Actualizamos el Stock de cada vacuna
+        vac.setStock(len(dosis))
+        #Eliminamos de la lista de variables el par de vacuna - dosis ya utilizado
+        var.pop(0)
+        var.pop(0)
    
-    #hago un bucle para que cada cola se llene del numero de vacunas llegadas
-    for i in range(vac_a.getEntrada()):
-        dosis_a.enqueue(1)
-
-    for i in range(vac_b.getEntrada()):
-        dosis_b.enqueue(1)
-    
-    for i in range(vac_c.getEntrada()):
-        dosis_c.enqueue(1)
-    
-    vac_a.setStock(len(dosis_a))
-    vac_b.setStock(len(dosis_b))
-    vac_c.setStock(len(dosis_c))
     
     
 def llegadapersonas(personas_dia, pacientes):
 
-   """Funcion encargada de crear la cola de gente de edades aleatorias uniformemente"""
-   #creo una cola de personas con edades aleatorias
+   """Funcion encargada de crear la cola de
+   gente de edades aleatorias uniformemente."""
    
+   # Creamos una cola de personas con edades aleatorias
    for i in range(personas_dia):
        pacientes.enqueue(random.randint(0, 100))
 
